8397ed7990e4358792a3175c401a7a42d8aed065
JayasuryaJayaram <jayasurya.j@quadrasystems.net>
2023-12-29 14:46:49 +0530

    Webpart's responsive work

--- src/webparts/form/components/Form.tsx
+++ src/webparts/form/components/Form.tsx
@@ -18,28 +18,43 @@ export const Form = (props: IFormProps) => {
 
   return (
     <>
-      <div>
-        <h3 className={styles.formTitle}>Anonymous Suggestion</h3>
-      </div>
-      {list.map((item: any) => (
-        <section>
-          <div style={{ marginTop: "54px" }}>
-            <label className={styles.labelStyle}>
-              {item.Q_No}.{item.Questions}
-            </label>
+      <section className={styles.container}>
+        <div className={styles.column1}>
+          <img
+            src={require("../assets/Form_img.svg")}
+            alt="image should be here"
+            style={{ marginBottom: "30px" }}
+          />
+          <p className={styles.header}>Share without revealing</p>
+          <p className={styles.description}>
+            Your thoughts, your voice, your anonymity
+          </p>
+        </div>
+        <div className={styles.column2}>
+          <div>
+            <h3 className={styles.formTitle}>Anonymous Suggestion</h3>
           </div>
-          <div style={{ marginTop: "26px", marginLeft: "26px" }}>
-            <input
-              type="text"
-              placeholder="   Enter your answer"
-              className={styles.inputField}
-            />
+          {list.map((item: any) => (
+            <section>
+              <div style={{ marginTop: "54px" }}>
+                <label className={styles.labelStyle}>
+                  {item.Q_No}.{item.Questions}
+                </label>
+              </div>
+              <div style={{ marginTop: "26px" }}>
+                <input
+                  type="textarea"
+                  placeholder="   Enter your answer"
+                  className={styles.inputField}
+                />
+              </div>
+            </section>
+          ))}
+          <div style={{ marginTop: "40px" }}>
+            <button className={styles.submitButton}>Submit</button>
           </div>
-        </section>
-      ))}
-      <div style={{ marginTop: "80px", marginLeft: "26px" }}>
-        <button className={styles.submitButton}>Submit</button>
-      </div>
+        </div>
+      </section>
     </>
   );
 };
